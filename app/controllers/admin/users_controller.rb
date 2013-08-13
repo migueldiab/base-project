2385378b1d688c8035865cfc0e0eb86c0e8ce7c5
cherta <gchertok@gmail.com>
2013-08-13 13:36:16 -0300

    whitelisted the role for admin/users_controller and showing the role in the users list for admins

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -42,6 +42,6 @@ class Admin::UsersController < AdminController
   private
   
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :role)
   end
 end
