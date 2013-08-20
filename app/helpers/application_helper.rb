726d37ce8b6b0e7c06c151181e21733a35d92859
cherta <gchertok@gmail.com>
2013-08-20 16:43:55 -0300

    cleaning up the layouts and created a helper for the flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def yield_flash(flash)
+    unless flash.empty?
+      html = content_tag :div, class: "row" do
+        html = ""
+        flash.each do |name, msg|
+          html += content_tag :div, class: "col-12" do
+            content_tag :div, msg.html_safe, :id => "flash_#{name}", class: "alert alert-block"
+          end
+          html.html_safe
+        end
+        html.html_safe
+      end
+      html.html_safe
+    end
+  end
+
 end
